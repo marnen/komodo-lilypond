05581bdf00ef0826ead9b493b8ed879266ccf6d4
Marnen Laibow-Koser <marnen@marnen.org>
2010-09-28 17:48:09 -0400

    koext startlang Lilypond --ext .ly

---
+++ components/koLilypond_UDL_Language.py
@@ -0,0 +1,47 @@
+# Komodo Lilypond language service.
+
+import logging
+from koUDLLanguageBase import KoUDLLanguage
+
+
+log = logging.getLogger("koLilypondLanguage")
+#log.setLevel(logging.DEBUG)
+
+
+def registerLanguage(registry):
+    log.debug("Registering language Lilypond")
+    registry.registerLanguage(KoLilypondLanguage())
+
+
+class KoLilypondLanguage(KoUDLLanguage):
+    name = "Lilypond"
+    lexresLangName = "Lilypond"
+    _reg_desc_ = "%s Language" % name
+    _reg_contractid_ = "@activestate.com/koLanguage?language=%s;1" % name
+    _reg_clsid_ = "e6eef4be-5d66-4dbf-8036-961bd07781df"
+    defaultExtension = '.ly'
+
+    #TODO: Update 'lang_from_udl_family' as appropriate for your
+    #      lexer definition. There are four UDL language families:
+    #           M (markup), i.e. HTML or XML
+    #           CSL (client-side language), e.g. JavaScript
+    #           SSL (server-side language), e.g. Perl, PHP, Python
+    #           TPL (template language), e.g. RHTML, Django, Smarty
+    #      'lang_from_udl_family' maps each UDL family code (M,
+    #      CSL, ...) to the sub-langauge name in your language.
+    #      Some examples:
+    #        lang_from_udl_family = {   # A PHP file can contain
+    #           'M': 'HTML',            #   HTML
+    #           'SSL': 'PHP',           #   PHP
+    #           'CSL': 'JavaScript',    #   JavaScript
+    #        }
+    #        lang_from_udl_family = {   # An RHTML file can contain
+    #           'M': 'HTML',            #   HTML
+    #           'SSL': 'Ruby',          #   Ruby
+    #           'CSL': 'JavaScript',    #   JavaScript
+    #           'TPL': 'RHTML',         #   RHTML template code
+    #        }
+    #        lang_from_udl_family = {   # A plain XML can just contain
+    #           'M': 'XML',             #   XML
+    #        }
+    lang_from_udl_family = {}
